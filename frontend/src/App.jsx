0f82e8f3d21457474e7151823dbc608da51496ac
Zara Khan <zarakhan.99@hotmail.com>
2025-04-24 01:56:32 +0100

    Updated Footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,17 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css'
 import Account from './pages/Account';
 import { Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
-        <Route path="/" element={<Navigate to="/account" />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/account" element={<Account />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
--- frontend/src/pages/HomePage.jsx
+++
@@ -1,5 +0,0 @@
-function Home() {
-    return <h1>Welcome to Book Review App 📚</h1>;
-  }
-  export default Home;
-  
