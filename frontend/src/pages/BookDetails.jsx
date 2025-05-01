3795bf6f117d9943c68ee95093182c71ad609133
Zara Khan <zarakhan.99@hotmail.com>
2025-05-01 18:44:32 +0100

    Updated book details to include stars for average rating

--- frontend/src/pages/BookDetails.jsx
+++ frontend/src/pages/BookDetails.jsx
@@ -8,23 +8,50 @@ const BookDetails = () => {
 
    const {bookId} = useParams();
 
-   const averageRating = () => {
-    if (!reviews && reviews > 1)
-    {
-        {reviews.map(review.rating) => (
+   const calcAverageRating = () => {
 
-        )
+    if (reviews && reviews.length > 0)
+        {
+            const bookRatings = reviews.map((review) => review.rating);
+            
+            const ratingSum = bookRatings.reduce((acc, rating) => acc + rating, 0);
+            
+            const averageRating = ratingSum /  bookRatings.length;
+
+            const stars = [];
+
+      // Loop through 1 to 5 stars
+    for (let i = 1; i <= 5; i++) {
+        if (averageRating >= i) {
+          stars.push(<FaStar key={i} />);
+        } else if (averageRating >= i - 0.5) {
+          stars.push(<FaStarHalfAlt key={i} />);
+        } else {
+          stars.push(<FaRegStar key={i} />);
+        }
+      }
+  
+      return stars;
     }
-   }
+     else
+     {
+        return Array(5).fill().map((_, i) => <FaRegStar key={i} />);
+     }
+
+    }
+    
 
  
    return (
     <div>
         <h2>{BookDetails.title}</h2>
         <h3>{BookDetails.author}</h3>
+
         <p>{BookDetails.description}</p>
+
     </div>
    )
+
    
 
 
