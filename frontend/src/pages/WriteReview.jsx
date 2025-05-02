c07fd27a70554f35b3e747846f2b2f9dd0aea1e4
Zara Khan <zarakhan.99@hotmail.com>
2025-05-03 00:21:33 +0100

    Updated BookDetails to include button for user to write a review

---
+++ frontend/src/pages/WriteReview.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+import { useAppContext } from "../context/AppContext";
+import { useParams } from 'react-router-dom';
+
+const UserReview= () => {
+
+    const {bookId} = useParams();
+
+}
