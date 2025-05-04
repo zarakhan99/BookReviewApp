a02b23d5d46a93d35f5bb15fae188c7f65bffbf4
Zara Khan <zarakhan.99@hotmail.com>
2025-05-04 02:46:52 +0100

    updated user review form and star ratings

--- frontend/src/pages/WriteReview.jsx
+++ frontend/src/pages/WriteReview.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useAppContext } from "../context/AppContext";
 
@@ -13,6 +13,19 @@ const WriteReview= () => {
 
     const navigate = useNavigate();
     const {bookId} = useParams();
+
+    useEffect(() => {
+
+    const isAuthorized = () => {
+        if (!isAuthenticated)
+        {
+            navigate(`/account`)
+        }  
+    };
+    isAuthorized();
+
+    }, [isAuthenticated]);
+    
 
     const handleReviewsubmission = async () => {
 
@@ -35,15 +48,47 @@ const WriteReview= () => {
        
     }
 
+    const userStarRating = () => {
+
+        const handleClickStar = value => {
+            setRating(value)
+        };
+
+        const stars = [];
+        
+              // Loop through 1 to 5 stars
+            for (let i = 1; i <= 5; i++) {
+                if (i <= rating) 
+                {
+                    stars.push(<FaStar key={i} onClick={() => handleClickStar(i)}/>);
+    
+                }
+                else {
+                    stars.push(<FaRegStar key={i} onClick={() => handleClickStar(i)}/>); 
+                }
+            }
+            return stars;
+        }
+
     return
     (
         <div className ="reviews-wrapper">
             <div className='title'> 
                 <h3>Write a Review!</h3>
             </div>
-            <form>
+            <form className = "review-form">
+            <label for="memberId">First Name</label>
+            <label for="comment">What did you think?</label>
+            <textarea id="userComment" name="subject" placeholder="Enter review..."> 
+                <input onChange={reviewComment} > </input>
+            </textarea> 
+            <button type="submit" className="button">
+                Post
+                onClick={() => handleReviewsubmission()}
 
+            </button>
             </form>
+            
             
         </div>
         
