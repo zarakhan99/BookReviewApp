756f0a312de4fce58a61b314004d80592fe1b22e
Zara Khan <zarakhan.99@hotmail.com>
2025-05-07 17:42:28 +0100

    added nre migration with image url added to books

---
+++ backend/Migrations/20250507141815_AddImageUrlToBook.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookReviewApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageUrlToBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Books",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Books");
+        }
+    }
+}
