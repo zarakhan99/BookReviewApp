8e9797401054f6861ad04e6f1587d25d5d8359cc
Zara Khan <zarakhan.99@hotmail.com>
2025-05-08 18:23:58 +0100

    Updated Home description

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,18 @@
 function Home() {
-    return <h1>Welcome to BookLook </h1>;
+    return (
+      <div className ="home-container">
+      <div className="title">
+      <h1>Welcome to BookLook </h1>
+      <div className="sub-headline">
+        <h2> Discover, Rate & Review Your Favorite Reads</h2>
+      </div>
+      </div>
+      <div className="info">
+        <h3>BookLook is your community‑powered haven for book lovers. Whether you’re hunting for your next page‑turner or eager to share your thoughts on a recent read, BookLook makes it easy to rate, review, and discover books you’ll love. </h3>
+      </div>
+      </div>
+
+    );
   }
   export default Home;
   
