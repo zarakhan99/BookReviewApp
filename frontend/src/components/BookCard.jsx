e7f0a443b10d4a2409441e0d70a20b0014b84dee
Zara Khan <zarakhan.99@hotmail.com>
2025-05-08 18:23:01 +0100

    fixed book card distoration and removed description

--- frontend/src/components/BookCard.jsx
+++ frontend/src/components/BookCard.jsx
@@ -11,19 +11,25 @@ function BookCard({ bookId, title, author, bookDescription, imageUrl }) {
     const previewLength = bookDescription.length > 110
   ? bookDescription.substring(0, 110) + '...' : bookDescription
 
-    return (
-        <div className="BookCard">
-          <img
+  return (
+    <div className="BookCard">
+      <div className="book-cover-container">
+        <img
           src={imageUrl?.startsWith('http') ? imageUrl : `http://localhost:5276${imageUrl}`}
           alt={title}
           className="book-cover"
-          />
-          <h4>{title}</h4>
-          <p> Author: {author}</p>
-          <p>{previewLength}</p>
-          <Link to={`/books/${bookId}`} className = "book-details">Book Details</Link>
+        />
+      </div>
+
+      <div className="book-info">
+        <h4>{title}</h4>
+        <p>Author: {author}</p>
+        {/*<p>{previewLength}</p> */}
         </div>
-      );
+        
+        <Link to={`/books/${bookId}`} className="book-details">Book Details</Link>
+    </div>
+);
     }
     
   export default BookCard;
