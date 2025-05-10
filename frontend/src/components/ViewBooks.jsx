05d6fbab313c315a891a63f029b50509da4f9378
Zara Khan <zarakhan.99@hotmail.com>
2025-05-10 17:10:09 +0100

    Created new component

---
+++ frontend/src/components/ViewBooks.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react';
+import { useAppContext } from "../context/AppContext";
+
+const ViewBooks = () => {
+
+    const { books, genres, fetchBooksByGenre, error, loading} = useAppContext();
+
+    return
+
+}
